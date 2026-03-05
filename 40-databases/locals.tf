73192e708d389ecc5eba4428dd55bfdcd5cb95a4
satishlakamsani <lakamsanisatish@gmail.com>
2026-03-05 20:50:05 +0530

    added files

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -10,4 +10,5 @@ locals {
     #public subnet in 1AZ
     database_subnet_id = split(",",data.aws_ssm_parameter.database_subnet_ids.value)[0]
     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
+    redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
 }
--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -5,3 +5,11 @@ variable "project"{
 variable "environment"{
     default = "dev"
 }
+
+variable "zone_id"{
+    default = "Z0031972RV2QBNE1P8DT"
+}
+
+variable "domain_name"{
+    default = "pugasaha.online"
+}
