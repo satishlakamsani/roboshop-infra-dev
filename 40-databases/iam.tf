0ee2c10ac3875a36c729b3e5929a16d5e3da2c5d
satishlakamsani <lakamsanisatish@gmail.com>
2026-03-06 17:04:39 +0530

    added terraform

--- 40-databases/iam.tf
+++ 40-databases/iam.tf
@@ -3,49 +3,38 @@ resource "aws_iam_role" "mysql" {
 
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
-
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
+        Action = "sts:AssumeRole"
         Effect = "Allow"
+        Sid    = ""
         Principal = {
           Service = "ec2.amazonaws.com"
         }
-        Action = "sts:AssumeRole"
-      }
+      },
     ]
   })
 
   tags = merge(
     {
-
-  Name = local.mysql_role_name
+        Name = local.mysql_role_name
     },
     local.common_tags
-
   )
 }
 
-
-
-# 2. Create the IAM policy resource using the JSON from the data source
 resource "aws_iam_policy" "mysql" {
   name        = local.mysql_policy_name
-  description = "A policy for MYSQL EC2 instance"
-  policy      = templatefile(
-    "mysql-iam-policy.json",{
-        environment = var.environment
-    }
-  ) # Reference the JSON output
+  description = "A policy for MySQL Ec2 instance"
+  policy      = templatefile("mysql-iam-policy.json", {
+                environment = var.environment
+  })
 }
 
-
-
-resource "aws_iam_policy_attachment" "mysql" {
-  
-  role      = aws_iam_role.mysql.name
+resource "aws_iam_role_policy_attachment" "mysql" {
+  role       = aws_iam_role.mysql.name
   policy_arn = aws_iam_policy.mysql.arn
 }
 
