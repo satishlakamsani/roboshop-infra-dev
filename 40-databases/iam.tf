c7397ae8adb5c4678a1bfa64983d33b8fb6af627
satishlakamsani <lakamsanisatish@gmail.com>
2026-03-06 17:30:02 +0530

    added terraform

--- 40-databases/iam.tf
+++ 40-databases/iam.tf
@@ -3,6 +3,8 @@ resource "aws_iam_role" "mysql" {
 
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
+
+  
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
