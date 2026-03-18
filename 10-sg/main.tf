0631ce31f42c0e96d319d2c94111b3b80e251d78
satishlakamsani <lakamsanisatish@gmail.com>
2026-03-18 10:58:41 +0530

    added terraform

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -3,6 +3,6 @@ module "sg"{
     source = "../../terraform_aws_sg"
     project = var.project
     environment = var.environment
-     sg_name = replace(var.sg_names[count.index],"_","-")
+    sg_name = replace(var.sg_names[count.index],"_","-")
     vpc_id = local.vpc_id
 }
