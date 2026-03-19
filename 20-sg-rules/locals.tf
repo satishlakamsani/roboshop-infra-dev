05c49a6ae383944ad02733c204e3d75a2e7df188
satishlakamsani <lakamsanisatish@gmail.com>
2026-03-19 12:15:59 +0530

    added rule

--- 20-sg-rules/locals.tf
+++ 20-sg-rules/locals.tf
@@ -10,6 +10,7 @@ locals {
   redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
   mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
   rabbitmq_sg_id = data.aws_ssm_parameter.rabbitmq_sg_id.value
+  frontend_sg_id = data.aws_ssm_parameter.frontend_sg_id.value
   backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
   frontend_alb_sg_id = data.aws_ssm_parameter.frontend_alb_sg_id.value
 }
